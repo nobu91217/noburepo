db269df01c786b4cfa8935217ee2123bbaa812e9
shimada <shimada@proadus.jp>
2016-07-06 18:56:11 +0900

    DbUtil.getConnection()を生成。簡単にConnectionが取れるよ。

--- src/jp/LoginServlet.java
+++ src/jp/LoginServlet.java
@@ -1,19 +1,18 @@
 package jp;
 
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
+
+import jp.nobu.util.DbUtil;
 
 
 
@@ -24,6 +23,11 @@ import javax.sql.DataSource;
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8118821188723764546L;
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		Connection db = null;
@@ -35,9 +39,8 @@ public class LoginServlet extends HttpServlet {
 		
 		try{
 			
-			Context context = new InitialContext();
-			DataSource ds = (DataSource)context.lookup("java:comp/env/jdbc/nobuweb");
-			db = (Connection) ds.getConnection();
+		
+			db = DbUtil.getConnection();
 			ps = db.prepareStatement("SELECT * FROM user_info where id = ? and password = ? ");
 			ps.setString(1, id);
 			ps.setString(2, pass);
@@ -46,10 +49,10 @@ public class LoginServlet extends HttpServlet {
 			
 			if(rs.next()) {
 				getServletContext().getRequestDispatcher("/loginok.jsp").forward(request, response);
-				//‘¶İ‚·‚éê‡‚Ìˆ—@¬Œ÷‰æ–Ê‚É‘JˆÚ
+				//ï¿½ï¿½ï¿½İ‚ï¿½ï¿½ï¿½ê‡ï¿½Ìï¿½ï¿½ï¿½ï¿½@ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ê‚É‘Jï¿½ï¿½
 			} else {
 				getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
-				//‘¶İ‚µ‚È‚¢ê‡‚Ìˆ— ƒƒOƒCƒ“‰æ–Ê‚É‘JˆÚ
+				//ï¿½ï¿½ï¿½İ‚ï¿½ï¿½È‚ï¿½ï¿½ê‡ï¿½Ìï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Oï¿½Cï¿½ï¿½ï¿½ï¿½Ê‚É‘Jï¿½ï¿½
 			}	
 
 		
---
+++ src/jp/nobu/NobuAppConfigureException.java
@@ -0,0 +1,25 @@
+package jp.nobu;
+
+public class NobuAppConfigureException extends RuntimeException {
+
+	private static final long serialVersionUID = 4565133750157390246L;
+
+	public NobuAppConfigureException() {
+	}
+
+	public NobuAppConfigureException(String msg) {
+		super(msg);
+	}
+
+	public NobuAppConfigureException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public static NobuAppConfigureException wrap(String msg, Throwable cause) {
+		if (cause instanceof NobuAppConfigureException) {
+			return (NobuAppConfigureException) cause;
+		}
+		throw new NobuAppConfigureException(msg, cause);
+	}
+
+}
