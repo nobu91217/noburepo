84009bfffcfd2d54e43f95bb1af36affe1efee4d
nobuhiko <momiki@proadus.jp>
2016-07-07 16:23:35 +0900

    "LoginServletを修正（2度目）"

--- src/jp/LoginServlet.java
+++ src/jp/LoginServlet.java
@@ -1,10 +1,6 @@
 package jp;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,11 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import jp.nobu.util.DbUtil;
-
-
-
-
+import jp.nobu.service.UserService;
 
 /**
  * Servlet implementation class LoginServlet
@@ -28,45 +20,19 @@ public class LoginServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 8118821188723764546L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		Connection db = null;
-		PreparedStatement ps =null;
-		ResultSet rs = null;
-		
-		String id = request.getParameter("id");
-		String pass = request.getParameter("pass");
-		request.setAttribute("id",id);
-		
-		try{
-			
-		
-			db = DbUtil.getConnection();
-			ps = db.prepareStatement("SELECT * FROM user_info where user_id = ? and password = ?");
-			ps.setString(1, id);
-			ps.setString(2, pass);
-			
-			rs = ps.executeQuery();
-			
-			if(rs.next()) {
-				getServletContext().getRequestDispatcher("/loginok.jsp").forward(request, response);
-				//存在する場合の処理　成功画面に遷移
-			} else {
-				getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
-				//存在しない場合の処理 ログイン画面に遷移
-			}	
 
-		
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try{
-				rs.close();
-				ps.close();
-				db.close();
-			}catch(SQLException ex) {
-				//inogre
-			}
+		if (UserService.INSTANCE.login(request.getParameter("id"), request.getParameter("pass"))) {
+			getServletContext().getRequestDispatcher("/loginok.jsp").forward(request, response);
+			// 存在する場合の処理 成功画面に遷移
+		} else {
+			getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+			// 存在しない場合の処理 ログイン画面に遷移
 		}
+
+		// inogre
 	}
+
 }
