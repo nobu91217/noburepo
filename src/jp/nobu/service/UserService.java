35399dbb0e0a9b84877b3e547f4e025a647e07d5
shimada <shimada@proadus.jp>
2016-07-07 19:12:05 +0900

    ユーザーサービスの更新と削除処理を定義

--- src/jp/nobu/service/UserService.java
+++ src/jp/nobu/service/UserService.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import jp.nobu.NobuSystemException;
 import jp.nobu.domain.User;
-import jp.nobu.util.DbUtil;
 
 /**
  * ユーザーに関するサービスを行う。
@@ -23,6 +22,15 @@ public class UserService extends GenericSearvice {
 
 	private UserService() {}
 
+	/**
+	 * ログインする。
+	 * 
+	 * @param userId
+	 *            ユーザID
+	 * @param password
+	 *            パスワード
+	 * @return 成功時trueを返す。
+	 */
 	public boolean login(String userId, String password) {
 
 		Connection con = null;
@@ -50,7 +58,12 @@ public class UserService extends GenericSearvice {
 			}
 		}
 	}
-	
+
+	/**
+	 * パスワードがnotnullなユーザーの一覧を取得する。
+	 * 
+	 * @return ユーザー一覧
+	 */
 	public List<User> getUsers() {
 
 		Connection con = null;
@@ -60,16 +73,16 @@ public class UserService extends GenericSearvice {
 		try {
 
 			con = getConnection();
-			ps = con.prepareStatement("SELECT user_id,name FROM user_info ORDER BY user_id");
+			ps = con.prepareStatement("SELECT user_id,name FROM user_info where password is not null ORDER BY user_id");
 
 			rs = ps.executeQuery();
-			
+
 			List<User> ret = new ArrayList<User>();
-			
-			while(rs.next()) {
-				ret.add(new User(rs.getString("user_id"),rs.getString("name")));
+
+			while (rs.next()) {
+				ret.add(new User(rs.getString("user_id"), rs.getString("name")));
 			}
-			
+
 			return ret;
 
 		} catch (Exception e) {
@@ -83,6 +96,30 @@ public class UserService extends GenericSearvice {
 				// inogre
 			}
 		}
+	}
+
+	/**
+	 * ユーザー名を変更する。
+	 * 
+	 * @param userId
+	 *            変更対象のユーザを特定するユーザID
+	 * @param userName
+	 *            変更する新しい名前
+	 * @return 該当するユーザが存在し、正しく変更できればtrueを返す。存在しない場合falseを返す。
+	 */
+	public boolean updateUserName(String userId, String userName) {
+		return false;
+	}
+
+	/**
+	 * ユーザを削除する。
+	 * 
+	 * @param userId
+	 *            削除対象のユーザを特定するユーザID
+	 * @return 該当するユーザが存在し、正しく削除できればtrueを返す。存在しない場合falseを返す。
+	 */
+	public boolean deleteUserByUserId(String userId) {
+		return false;
 	}
 
 }
